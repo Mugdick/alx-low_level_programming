e535544efc067f790b0e5ba06d5d6f587699bd9f
Aryatunga Dickens <aryadickens@gmail.com>
2023-10-26 11:58:49 +0300

    4-pow_recursion.c task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,23 +9,20 @@
  */
 
 int _pow_recursion(int x, int y)
+
 {
-	int result = x;
-
-	if (y < 0)
-{
-		return -1;
-}
-else if (y == 0)
-{
-
-		return 1;
-}
-else
-{ 
-
-	result *= _pow_recursion(x, y - 1);
-}
-
-	return (result);
+    int result = x;
+    if (y < 0)
+    {
+        return -1;
+    }
+    else if (y == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        result *= _pow_recursion(x, y - 1);
+    }
+    return (result);
 }
