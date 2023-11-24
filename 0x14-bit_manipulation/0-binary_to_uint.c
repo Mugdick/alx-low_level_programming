7a9b5251c10d0bf05efc7bd1703a3892991d50bf
Aryatunga dickens <aryadickens@gmail.com>
2023-11-24 13:38:38 +0300

    Bit Manupulation tasks done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,16 +14,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int len = 0;
+	int length = 0;
 
-	if (b[len] == '\0')
+	if (b[length] == '\0')
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
+	while ((b[length] == '0') || (b[length] == '1'))
 	{
 		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		num += b[length] - '0';
+		length++;
 	}
 
 	return (num);
